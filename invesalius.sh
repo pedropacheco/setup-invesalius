30fa91c8650300060dcbdfb21cc1ac57ef5400d1
Pedro Pacheco <pedrovitorpacheco@gmail.com>
2014-09-23 16:36:23 -0300

    Update invesalius.sh

--- invesalius.sh
+++ invesalius.sh
@@ -7,8 +7,10 @@
 #
 # Description: Script that installs all dependencies InVesalius 3.
 #
-# Usage: ./invesalius.sh
+# Usage: ./invesalius.sh or sh invesalius.sh
 # Alias: invesalius="bash ~/path/to/script/invesalius.sh"
+#
+# Ps: This script run only in systems that use apt-get as management packages.
 #
 # Important:
 #
@@ -17,59 +19,21 @@
 # ------------------------------------------------------------------------------
 
 
-# Ask for the administrator password upfront
-sudo -v
+# Add repositor.
 
+sudo deb http://us.archive.ubuntu.com/ubuntu trusty main universe
 
-# To update the local package index with the latest changes made in the repositories
-echo "→ Initializing the update packages"
-apt-get update 
+# Update your management package.
 
-setup() {
-	echo "→ Installing python numpy..."
-	apt-get install python-numpy
+sudo apt-get upate
 
-	echo "→ Installing python scipy..."
-	apt-get install python-scipy
+# Install InVesalius.
 
-	echo "→ Installing python wxgtk 2.8..."
-	apt-get install python-wxgtk2.8
+sudo apt-get install invesalius
 
-	echo "→ Installing python imaging..."
-	apt-get install python-imaging
-
-	echo "→ Installing python vtk..."
-	apt-get install python-vtk
-
-	echo "→ Installing python gdcm..."
-	apt-get install python-gdcm
-
-	echo "→ Installing python vtkgdcm..."
-	apt-get install python-vtkgdcm
-
-	echo "→ Installing python casmoothing..."
-	apt-get install python-casmoothing
-
-	echo "→ Installing python nibabel..."
-	apt-get install python-nibabel
-
-	echo "→ Installing python serial..."
-	apt-get install python-serial
-
-	echo "→ Installing python psutil..."
-	apt-get install python-psutil
-
-	echo "→ Installing python skimage..."
-	apt-get install python-skimage
-
-	echo "→ Installing cython..."
-	apt-get install cython
-
-	echo "→ Installing git..."
-	apt-get install git
-
-	exit 0
-}
+echo InVesalius installed!
 
 # Initialize
+
 setup
+
